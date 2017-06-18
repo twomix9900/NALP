3cfa06246f3cba89e2bfef8d58bcd5bcae0e2d3f
Sho Nguyen <sho.nozaki417@gmail.com>
2017-06-17 17:17:57 -0700

    (added) partials for states, and (added) login

--- public/config.js
+++ public/config.js
@@ -11,17 +11,32 @@
 
   function config($stateProvider, $urlRouterProvider, $locationProvider, angularAuth0Provider) {
     
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/login');
 
     $stateProvider
     .state('login', {
-      url: '/',
+      url: '/login',
       templateUrl: 'partials/login.html',
       controller: 'loginCtrl as login_ctrl'
     })
+    .state('search', {
+      url: '/search',
+      templateUrl: 'partials/search.html',
+      controller: 'searchCtrl as search_ctrl'
+    })
+    .state('create', {
+      url: '/create',
+      templateUrl: 'partials/create.html',
+      controller: 'createCtrl as create_ctrl'
+    })
+    .state('plan', {
+      url: '/plan',
+      templateUrl: 'partials/plan.html',
+      controller: 'planCtrl as plan_ctrl'
+    })
     .state('callback', {
       url: '/callback',
-      templateUrl: 'partials/callback.html',
+      templateUrl: 'partials/search.html',
       controller: 'callbackCtrl as callback_ctrl'
     });
 
@@ -30,7 +45,7 @@
       domain: 'shonozaki.auth0.com',
       responseType: 'token id_token',
       audience: 'https://shonozaki.auth0.com/userinfo',
-      redirectUri: 'http://localhost:3002/',
+      redirectUri: 'http://localhost:3002/#/callback',
       scope: 'openid'
     });
 
--- public/factories_services/authService.js
+++ public/factories_services/authService.js
@@ -9,7 +9,48 @@
       angularAuth0.authorize();
     }
 
-    return { login: login };
+    function handleAuthentication() {
+      angularAuth0.parseHash(function(err, authResult) {
+        if(authResult && authResult.accessToken && authResult.idToken) {
+          setSession(authResult);
+          $state.go('search');
+        } else if (err) {
+          $timeout(function() {
+            $state.go('search');
+          });
+          console.log(err);
+        }
+      })
+    }
+
+    function setSession(authResult) {
+      //sets time access token will expire
+      let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+      localStorage.setItem('access_token', authResult.accessToken);
+      localStorage.setItem('id_token', authResult.idToken);
+      localStorage.setItem('expires_at', expiresAt);
+    }
+    
+    function logout() {
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('id_token');
+      localStorage.removeItem('expires_at');
+    }
+
+    function isAuthenticated() {
+      //check if token is past expire time
+      let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+      return new Date().getTime() > expiresAt;
+    }
+
+
+    return { 
+      login: login,
+      handleAuthentication: handleAuthentication,
+      setSession: setSession,
+      logout: logout,
+      isAuthenticated: isAuthenticated
+    };
   }
 
 })();
