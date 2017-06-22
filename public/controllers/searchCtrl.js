e0e76a1b27b6949fb7f0879e077e204072e11327
Sho Nguyen <sho.nozaki417@gmail.com>
2017-06-22 14:12:32 -0700

    removed dummy data

--- public/controllers/searchCtrl.js
+++ public/controllers/searchCtrl.js
@@ -7,27 +7,18 @@
   function searchCtrl(plan_fac, $state) {
     var vm = this;
 
-    vm.dummydata = [
-      {title: 'Best LA'},
-      {title: 'Best NYC'},
-      {title: 'Best Japan'},
-      {title: 'Best Miami'},
-      {title: 'Food it up'},
-      {title: 'yeezy'},
-      {title: 'hypebeast'},
-    ];
+    vm.plans = [];
+
     vm.clickGetPlans = function() {
       plan_fac.getAllPlans()
       .then((plans) => {
         console.log(plans.data.plans)
-        vm.dummydata = plans.data.plans;
+        vm.data = plans.data.plans;
       }).catch((err) => {
         console.log('error getting plans', err);
       })
     }
     vm.goToPlan = function(p_id) {
-      console.log('p_id', p_id)
-      
       $state.go('plan', { id: p_id });
     }
   }
