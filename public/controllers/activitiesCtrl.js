4e28e35771381730a305e0c27fcd43b9cbf8a056
bananafish2828 <bananafish2828@gmail.com>
2017-06-21 23:00:38 -0700

    activities wip

--- public/controllers/activitiesCtrl.js
+++ public/controllers/activitiesCtrl.js
@@ -2,31 +2,40 @@
   angular.module('NALP')
   .controller('activitiesCtrl', activitiesCtrl)
 
-  activitiesCtrl.$inject = ['plans_fac'];
+  activitiesCtrl.$inject = ['selectedPlans_fac'];
 
-  function activitiesCtrl(plans_fac) {
+  function activitiesCtrl(selectedPlans_fac) {
     var vm = this;
 
-    vm.userClick = function(){
-
-
-
+    vm.userClickBookmarked = function(){
+      console.log('i got bookmarked');
     };
-    
-    plans_fac.getPlans
 
+    vm.userClickCreated = function() {
+      console.log('i got created');
+    };
 
+    vm.userClickCompleted = function() {
+      console.log('i got rated');
+    };
 
+    vm.userHover = function() {
+    // highlight selection  
+    };
+    //dummy data
+    var option = 'bookmarked'
+    var id = '594ae6c148ec2d34688f7c87'
 
+    selectedPlans_fac.getPlans(option, id)
+      .then(renderPlans, function(err) {
+        if (err) throw err;
+      });
+
+    function renderPlans(res) {
+      console.log('res data ', res);
+      vm.plans = res.data.plans;
+    }
+  
   }
-
-
-
-
-
-
-
-
-
 
 })();
