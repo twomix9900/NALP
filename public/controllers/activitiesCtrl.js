360ca46b18ab0975900e26eb9b0fab81a9fe0496
bananafish2828 <bananafish2828@gmail.com>
2017-06-21 18:12:25 -0700

    (add) activities factory

--- public/controllers/activitiesCtrl.js
+++ public/controllers/activitiesCtrl.js
@@ -2,10 +2,19 @@
   angular.module('NALP')
   .controller('activitiesCtrl', activitiesCtrl)
 
-  activitiesCtrl.$inject = ['plans'];
+  activitiesCtrl.$inject = ['plans_fac'];
 
-  function activitiesCtrl(plans) {
+  function activitiesCtrl(plans_fac) {
+    var vm = this;
+
+    vm.userClick = function(){
+
+
+
+    };
     
+    plans_fac.getPlans
+
 
 
 
