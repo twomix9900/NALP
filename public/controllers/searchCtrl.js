52f1759d048c2f0b60ff5464f051c5d733f4dee5
Sho Nguyen <sho.nozaki417@gmail.com>
2017-06-22 11:04:49 -0700

    wokring on search

--- public/controllers/searchCtrl.js
+++ public/controllers/searchCtrl.js
@@ -5,6 +5,17 @@
   searchCtrl.$inject = [];
 
   function searchCtrl() {
+    var vm = this;
+
+    vm.dummydata = [
+      {title: 'Best LA'},
+      {title: 'Best NYC'},
+      {title: 'Best Japan'},
+      {title: 'Best Miami'},
+      {title: 'Food it up'},
+      {title: 'yeezy'},
+      {title: 'hypebeast'},
+    ];
     
   }
 })()
