917fa8569d9c723187016a03d3236555115655fe
lewhub <bracey.lewis@gmail.com>
2017-06-23 12:40:13 -0700

    (fix) removed password from user schema

--- models/user.js
+++ models/user.js
@@ -2,9 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
 var userSchema = Schema({
-
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
   rated_plans: [ { type: Schema.Types.ObjectId, ref: 'Plan' } ],
   created_plans: [ { type: Schema.Types.ObjectId, ref: 'Plan' } ],
   bookmark_plans: [ {type: Schema.Types.ObjectId, ref: 'Plan'} ]
