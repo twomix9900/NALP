9b7688bc6525844aa711eacdf70d8fab907bb347
lewhub <bracey.lewis@gmail.com>
2017-06-23 13:25:21 -0700

    (fix) catching error in navbar.js is now working

--- router/user_router.js
+++ router/user_router.js
@@ -6,6 +6,7 @@ const UserController = require('../controllers/user_ctrl.js');
 userRouter.get('/', UserController.index);
 userRouter.post('/', UserController.create);
 userRouter.get('/:id', UserController.show);
+userRouter.get('/email/:email', UserController.show_email);
 userRouter.put('/:id', UserController.update_user);
 userRouter.delete('/:id', UserController.delete_user);
 
