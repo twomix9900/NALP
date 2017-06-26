3e561e7e598a01f2f894efaa3e6a25c386dde1d4
bananafish2828 <bananafish2828@gmail.com>
2017-06-26 14:29:36 -0700

    returns it to pre bootstrap

--- public/controllers/activitiesCtrl.js
+++ public/controllers/activitiesCtrl.js
@@ -7,7 +7,7 @@
   function activitiesCtrl(selectedPlans_fac, $state, store, auth_fac) {
     auth_fac.private();
     
-  //  document.body.style.backgroundImage = "url('../backgrounds/roller-skates.jpg')";
+    document.body.style.backgroundImage = "url('../assets/img/roller-skates.jpg')";
     
     var vm = this;
     var viewtitle = '';
